2304ce4c89e26fc7c90a58029e6de81758f148ff
Sanatrr46 <74090262+Sanatrr46@users.noreply.github.com>
2022-03-08 15:05:21 +0530

    Calculator

---
+++ main.py
@@ -0,0 +1,11 @@
+
+a=int(input("Your First Number:"))
+b=int(input("Your Second Number:"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Your addition is:",c)
+print("Your substraction is:",d)
+print("Your multiplication is:",e)
+print("Your division is:",f)
